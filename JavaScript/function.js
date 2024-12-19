8854d6239a77612efa13ec36ef9318fa29e33f76
Muhammad Muzammil <yoursmuzammil16070@gmail.com>
2024-12-19 15:24:26 +0500

    first commit

---
+++ JavaScript/function.js
@@ -0,0 +1,72 @@
+
+
+var a = 5
+number()
+Number2()
+console.log(a);
+
+function number() {
+    var a = 10;
+    console.log(a)
+}
+
+function Number2 (){
+    var a = 20;
+    console.log(a);
+    
+}
+
+
+function addToNumber(a, b){
+     return `${a+b}`
+
+}
+console.log(addToNumber(5,5))
+
+function multiTownumber(a,b) {
+    return `${a*b}`
+}
+
+console.log(multiTownumber(3,2))
+
+
+function divideToNum (number1, number2) {
+    return number1/number2
+}
+console.log(divideToNum(10,2));
+
+
+function isPositive (num) {
+    if (num === true) {
+        console.log("Instead of Mt True It Is False")
+    }
+    return num > 0
+}
+console.log(isPositive(-2))
+
+
+function funcName () {
+    for(let i=1; i<=100; i++)
+        console.log(i)
+}
+funcName()
+
+function getAverage(num1, num2) {
+    console.log ("The Average Value Are :", (num1 + num2)/2)
+}
+getAverage(50,5)
+
+
+// sum of three value in a function
+function SumAllVal (a,b,c) {
+    console.log("The Sum Of All These Value Are : ", a+b+c);
+}
+SumAllVal(2,3,3)
+
+// Full name in Function
+
+function GetFullName (firstN, LastN) {
+    let FullName = firstN+ " " + LastN
+    console.log(FullName)
+}
+GetFullName("Muhammad", "Muzammil")
